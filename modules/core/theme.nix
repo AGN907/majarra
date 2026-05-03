0a499abcc27630d6c27b558edb3f0abf27d17e08
agn907 <ahmednahdi907@gmail.com>
2026-05-04 02:35:36 +0300

    add theme module under namespace `nawa.core`

---
+++ modules/core/theme.nix
@@ -0,0 +1,35 @@
+{ inputs, nawa, ... }:
+{
+  flake-file.inputs.stylix.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  nawa.core.includes = [ nawa.core._.theme ];
+
+  nawa.core._.theme = {
+    nixos =
+      { pkgs, ... }:
+      {
+        imports = [ inputs.stylix.nixosModules.stylix ];
+        fonts = {
+          packages = with pkgs; [
+            maple-mono.NF
+            noto-fonts
+            noto-fonts-cjk-sans
+            noto-fonts-color-emoji
+            nerd-fonts.symbols-only
+            liberation_ttf
+            inter
+          ];
+          enableDefaultPackages = true;
+          fontDir.enable = true;
+          fontconfig.defaultFonts = {
+            serif = [ "Liberation Serif" ];
+            sansSerif = [ "Inter" ];
+            monospace = [ "Maple Mono NF" ];
+          };
+        };
+      };
+    homeManager = {
+      imports = [ inputs.stylix.homeModules.stylix ];
+    };
+  };
+}
