22070eafe92a8c76fe2548f43f41029027efc42a
agn907 <ahmednahdi907@gmail.com>
2026-05-05 02:20:42 +0300

    add cli-tools module under `nawa.apps`

---
+++ modules/apps/cli-tools.nix
@@ -0,0 +1,81 @@
+{
+  nawa.apps._.cliTools = {
+    homeManager =
+      { pkgs, config, ... }:
+      {
+        sops.secrets.atuin_key = { };
+
+        programs = {
+          zoxide = {
+            enable = true;
+            enableFishIntegration = true;
+          };
+          eza = {
+            enable = true;
+            enableFishIntegration = true;
+            git = true;
+            colors = "always";
+            icons = "always";
+          };
+          bat = {
+            enable = true;
+            extraPackages = with pkgs.bat-extras; [
+              batdiff
+              batgrep
+              prettybat
+            ];
+          };
+          atuin = {
+            enable = true;
+            flags = [ "--disable-up-arrow" ];
+            enableFishIntegration = true;
+            forceOverwriteSettings = true;
+            settings = {
+              dialect = "uk";
+              sync_address = "https://api.atuin.sh";
+              sync_frequency = "2h";
+              enter_accept = true;
+              key_path = config.sops.secrets.atuin_key.path;
+              keymap-mode = "vim-normal";
+              show_help = true;
+              stats = {
+                common_subcommands = [
+                  "cargo"
+                  "composer"
+                  "docker"
+                  "git"
+                  "npm"
+                  "pnpm"
+                  "systemctl"
+                  "zellij"
+                ];
+                ignored_commands = [
+                  "cd"
+                  "z"
+                  "ls"
+                  "vi"
+                ];
+              };
+              sync.records = true;
+              search.filters = [
+                "global"
+                "host"
+                "session"
+                "workspace"
+                "directory"
+              ];
+              dotfiles.enabled = true;
+            };
+          };
+          direnv = {
+            enable = true;
+            nix-direnv.enable = true;
+          };
+          tealdeer = {
+            enable = true;
+            settings.updates.auto_update = true;
+          };
+        };
+      };
+  };
+}
