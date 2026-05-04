613e9d20b26c849fc009b9bb3b3b942ec1e2190f
agn907 <ahmednahdi907@gmail.com>
2026-05-05 02:19:31 +0300

    add missing packages for yazi plugins

--- modules/apps/yazi.nix
+++ modules/apps/yazi.nix
@@ -23,6 +23,12 @@
           substituters = [ "https://yazi.cachix.org" ];
           trusted-public-keys = [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
         };
+        home.packages = with pkgs; [
+          trash-cli
+          ouch
+          mediainfo
+        ];
+
         programs.yazi = {
           enable = true;
           package = inputs.yazi.packages.${system}.default;
