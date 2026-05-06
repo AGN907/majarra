0b90b90f868cb55e4f63cbcc54dfcd4fcd935e43
agn907 <ahmednahdi907@gmail.com>
2026-05-06 17:21:09 +0300

    update agn modules

--- modules/agn.nix
+++ modules/agn.nix
@@ -7,11 +7,19 @@
   den.aspects.agn = {
     includes = [
       <den/primary-user>
-      
+
+      <nawa/core>
       <nawa/secrets>
+
+      <nawa/apps/zen>
+      <nawa/apps/kitty>
+      <nawa/apps/cliTools>
+      <nawa/apps/niri>
       <nawa/apps/neovim>
       <nawa/apps/yazi>
       <nawa/apps/zellij>
+
+      <nawa/services/syncthing/client>
     ];
   };
 }
