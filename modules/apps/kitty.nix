29e6d18f7ee00c3a415cb10c15ba7dc22827d2fd
agn907 <ahmednahdi907@gmail.com>
2026-05-07 17:25:17 +0300

    update kitty config

--- modules/apps/kitty.nix
+++ modules/apps/kitty.nix
@@ -4,18 +4,15 @@
     homeManager = {
       programs.kitty = {
         enable = true;
-        font = {
-          name = "Maple Mono NF";
-          size = 14;
-        };
         settings = {
           confirm_os_window_close = 0;
           dynamic_background_opacity = true;
           enable_audio_bell = false;
-          mouse_hide_wait = "-1.0";
           window_padding_width = 10;
           cursor_shape = "beam";
-          cursor_trail = 100;
+          cursor_trail = 3;
+          scrollback_lines = 10000;
+          copy_on_select = "clipboard";
         };
       };
     };
