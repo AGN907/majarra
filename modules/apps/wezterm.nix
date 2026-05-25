5c50ca76130c19d778459d08c1a3d6bed5cd33d5
AGN907 <ahmednahdi907@gmail.com>
2026-05-25 05:44:43 +0300

    set padding to zero in wezterm module

--- modules/apps/wezterm.nix
+++ modules/apps/wezterm.nix
@@ -35,9 +35,9 @@
               enable_tab_bar = false;
               bidi_enabled = true;
               window_padding = {
-                left = 4;
-                right = 4;
-                top = 4;
+                left = 0;
+                right = 0;
+                top = 0;
                 bottom = 0;
               };
               keys = [
