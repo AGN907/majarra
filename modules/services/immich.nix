23a0bc33958588b8fd9a496dbed68c55bc621797
AGN907 <ahmednahdi907@gmail.com>
2026-05-31 01:42:05 +0300

    feat: add immich module

---
+++ modules/services/immich.nix
@@ -0,0 +1,34 @@
+{
+  nawa.services._.immich = {
+    nixos =
+      { config, pkgs, ... }:
+      {
+        sops.secrets."restic/immich" = { };
+        sops.secrets.rclone = { };
+
+        services.restic.backups.immich = {
+          initialize = true;
+
+          repository = "rclone:gdrive:immich-backups";
+
+          passwordFile = config.sops.secrets."restic/immich".path;
+          rcloneConfigFile = config.sops.secrets.rclone.path;
+
+          paths = [ config.services.immich.mediaLocation ];
+
+          pruneOpts = [
+            "--keep-daily 7"
+            "--keep-weekly 4"
+            "--keep-monthly 12"
+          ];
+        };
+
+        services.immich = {
+          enable = true;
+          host = "0.0.0.0";
+          port = 2283;
+          openFirewall = true;
+        };
+      };
+  };
+}
