5299ad7392bcfe6f7e8044c4f7648482d206d9a8
AGN907 <ahmednahdi907@gmail.com>
2026-06-06 22:34:30 +0300

    feat(wezterm): remove manual stylix theming as nix-community/stylix#2274 was merged

--- modules/apps/starship.nix
+++ modules/apps/starship.nix
@@ -6,13 +6,12 @@
         enable = true;
         enableFishIntegration = true;
         settings = {
-          format = " $username\$hostname\$directory\$git_branch\$git_state\$git_status\$cmd_duration\$line_break\$character";
+          format = "$username\$hostname\$directory\$git_branch\$git_state\$git_status\$cmd_duration\$line_break\$character";
           add_newline = true;
           username = {
-            format = "[$user]($style) ";
+            format = "[$user]($style)";
           };
           character = {
-            format = " $symbol ";
             success_symbol = "[➜](bold green)";
             error_symbol = "[➜](bold red)";
           };
