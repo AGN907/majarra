8aba89d47c7a6d0d3da61a52b732f057517d7c63
AGN907 <ahmednahdi907@gmail.com>
2026-06-13 02:39:53 +0300

    feat(nvim): setup mini-indentscope with default config

--- config/neovim/plugin/30_mini.lua
+++ config/neovim/plugin/30_mini.lua
@@ -220,12 +220,7 @@ nixInfo.lze.load({
 		"mini.indentscope",
 		after = function()
 			local indentscope = require("mini.indentscope")
-			indentscope.setup({
-				draw = {
-					animation = indentscope.gen_animation.quadratic(),
-				},
-				symbol = "|",
-			})
+			indentscope.setup()
 		end,
 	},
 	{
