512adb74cf839530526def6d21b1cbc90bc1919e
AGN907 <ahmednahdi907@gmail.com>
2026-06-13 21:05:25 +0300

    feat: update wezterm config

--- modules/apps/wezterm.nix
+++ modules/apps/wezterm.nix
@@ -1,33 +1,32 @@
 { lib, ... }:
 {
   nawa.apps._.wezterm = {
-    homeManager =
-      {
-        stylix.targets.wezterm.enable = true;
+    homeManager = {
+      stylix.targets.wezterm.enable = true;
 
-        programs.wezterm = {
-          enable = true;
-          settings = {
-            line_height = 1.2;
-            force_reverse_video_cursor = true;
-            use_resize_increments = true;
-            enable_tab_bar = false;
-            bidi_enabled = true;
-            window_padding = {
-              left = 4;
-              right = 0;
-              top = 0;
-              bottom = 0;
-            };
-            keys = [
-              {
-                key = "Enter";
-                mods = "ALT";
-                action = lib.generators.mkLuaInline "wezterm.action.DisableDefaultAssignment";
-              }
-            ];
+      programs.wezterm = {
+        enable = true;
+        settings = {
+          force_reverse_video_cursor = true;
+          use_resize_increments = true;
+          window_decorations = "NONE";
+          enable_tab_bar = false;
+          bidi_enabled = true;
+          window_padding = {
+            left = 8;
+            right = 0;
+            top = 0;
+            bottom = 2;
           };
+          keys = [
+            {
+              key = "Enter";
+              mods = "ALT";
+              action = lib.generators.mkLuaInline "wezterm.action.DisableDefaultAssignment";
+            }
+          ];
         };
       };
+    };
   };
 }
