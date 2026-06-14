e5a605d3e393021c688a3930e11d8ad5f74acedc
AGN907 <ahmednahdi907@gmail.com>
2026-06-14 22:09:22 +0300

    feat(noctalia): configure theming with stylix

--- modules/apps/noctalia.nix
+++ modules/apps/noctalia.nix
@@ -17,6 +17,9 @@
 
     homeManager =
       { config, ... }:
+      let
+        inherit (config.stylix) fonts opacity;
+      in
       {
         imports = [
           inputs.noctalia.homeModules.default
@@ -25,7 +28,7 @@
           enable = true;
           settings = {
             shell = {
-              font_family = "Maple Mono NF";
+              font_family = fonts.sansSerif.name;
               lang = "en_GB";
               polkit_agent = true;
               panel = {
@@ -61,14 +64,15 @@
             };
             theme = {
               mode = "dark";
-              source = "community";
-              community_palette = "Kanagawa Dragon";
+              source = "custom";
+              custom_palette = "stylix";
             };
             bar = {
               main = {
                 enabled = true;
                 position = "top";
-                background_opacity = 0.80;
+                background_opacity = opacity.desktop;
+                capsule_opacity = opacity.desktop;
                 widget_spacing = 6;
                 margin_ends = 120;
                 start = [
@@ -96,6 +100,10 @@
             notification = {
               enable_daemon = true;
               position = "top_right";
+              background_opacity = opacity.popups;
+            };
+            osd = {
+              background_opacity = opacity.popups;
             };
             weather = {
               enabled = true;
@@ -118,6 +126,113 @@
                 hide_when_no_media = true;
               };
             };
+          };
+          customPalettes = {
+            stylix =
+              let
+                inherit (config.lib.stylix) colors;
+              in
+              {
+                dark = with colors.withHashtag; {
+                  mPrimary = base0B; # #8a9a7b
+                  mOnPrimary = base00; # #181616
+                  mSecondary = base0C; # #8ea4a2
+                  mOnSecondary = base00; # #181616
+                  mTertiary = base08; # #c4746e
+                  mOnTertiary = base00; # #181616
+                  mError = base08; # #c4746e
+                  mOnError = base00; # #181616
+                  mSurface = base00; # #181616
+                  mOnSurface = base05; # #c5c9c5
+                  mHover = base02; # #393836
+                  mOnHover = base05; # #c5c9c5
+                  mSurfaceVariant = base01; # #282727
+                  mOnSurfaceVariant = base06; # #c8c093
+                  mOutline = base03; # #625e5a
+                  mShadow = base00; # #181616
+
+                  terminal = {
+                    background = base00; # #181616
+                    foreground = base05; # #c5c9c5
+                    cursor = base05; # #c5c9c5
+                    cursorText = base00; # #181616
+                    selectionBg = base05; # #c5c9c5
+                    selectionFg = base00; # #181616
+
+                    normal = {
+                      black = base01; # #282727
+                      red = base08; # #c4746e
+                      green = base0B; # #8a9a7b
+                      yellow = base0A; # #c4b28a
+                      blue = base0D; # #8ba4b0
+                      magenta = base0E; # #a292a3
+                      cyan = base0C; # #8ea4a2
+                      white = base05; # #c5c9c5
+                    };
+
+                    bright = {
+                      black = base03; # #625e5a
+                      red = base08; # #c4746e
+                      green = base0B; # #8a9a7b
+                      yellow = base09; # #b6927b
+                      blue = base0D; # #8ba4b0
+                      magenta = base0E; # #a292a3
+                      cyan = base0C; # #8ea4a2
+                      white = base06; # #c8c093
+                    };
+                  };
+                };
+
+                light = with colors.withHashtag; {
+                  mPrimary = base0B; # #8a9a7b
+                  mOnPrimary = base05; # #c5c9c5
+                  mSecondary = base0C; # #8ea4a2
+                  mOnSecondary = base05; # #c5c9c5
+                  mTertiary = base08; # #c4746e
+                  mOnTertiary = base05; # #c5c9c5
+                  mError = base08; # #c4746e
+                  mOnError = base05; # #c5c9c5
+                  mSurface = base05; # #c5c9c5
+                  mOnSurface = base00; # #181616
+                  mHover = base06; # #c8c093
+                  mOnHover = base00; # #181616
+                  mSurfaceVariant = base06; # #c8c093
+                  mOnSurfaceVariant = base00; # #181616
+                  mOutline = base0B; # #8a9a7b
+                  mShadow = base04; # #737c73
+
+                  terminal = {
+                    background = base05; # #c5c9c5
+                    foreground = base00; # #181616
+                    cursor = base00; # #181616
+                    cursorText = base05; # #c5c9c5
+                    selectionBg = base00; # #181616
+                    selectionFg = base05; # #c5c9c5
+
+                    normal = {
+                      black = base05; # #c5c9c5
+                      red = base08; # #c4746e
+                      green = base0B; # #8a9a7b
+                      yellow = base0A; # #c4b28a
+                      blue = base0D; # #8ba4b0
+                      magenta = base0E; # #a292a3
+                      cyan = base0C; # #8ea4a2
+                      white = base00; # #181616
+                    };
+
+                    bright = {
+                      black = base04; # #737c73
+                      red = base08; # #c4746e
+                      green = base0B; # #8a9a7b
+                      yellow = base09; # #b6927b
+                      blue = base0D; # #8ba4b0
+                      magenta = base0E; # #a292a3
+                      cyan = base0C; # #8ea4a2
+                      white = base01; # #282727
+                    };
+                  };
+                };
+              };
           };
         };
       };
