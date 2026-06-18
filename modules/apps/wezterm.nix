bb8e75a293c344407d69b8c69a5f2b0db4f98ecd
AGN907 <ahmednahdi907@gmail.com>
2026-06-19 01:54:41 +0300

    feat: update wezterm config

--- modules/apps/wezterm.nix
+++ modules/apps/wezterm.nix
@@ -1,32 +1,46 @@
 { lib, ... }:
 {
   nawa.apps._.wezterm = {
-    homeManager = {
-      stylix.targets.wezterm.enable = true;
+    homeManager =
+      { config, ... }:
+      let
+        inherit (config.stylix) fonts;
+      in
+      {
+        stylix.targets.wezterm.enable = true;
 
-      programs.wezterm = {
-        enable = true;
-        settings = {
-          force_reverse_video_cursor = true;
-          use_resize_increments = true;
-          window_decorations = "NONE";
-          enable_tab_bar = false;
-          bidi_enabled = true;
-          window_padding = {
-            left = 8;
-            right = 0;
-            top = 0;
-            bottom = 2;
+        programs.wezterm = {
+          enable = true;
+          settings = {
+            font = lib.mkForce (
+              lib.generators.mkLuaInline ''
+                wezterm.font_with_fallback {
+                  { family = "${fonts.monospace.name}",weight = "Regular", italic = true },
+                  { family = "${fonts.emoji.name}"}
+                }''
+            );
+            default_cursor_style = "BlinkingBar";
+            cursor_blink_rate = 300;
+            force_reverse_video_cursor = true;
+            use_resize_increments = true;
+            window_decorations = "NONE";
+            enable_tab_bar = false;
+            bidi_enabled = true;
+            window_padding = {
+              left = "1cell";
+              right = "1cell";
+              top = "0.5cell";
+              bottom = "0.5cell";
+            };
+            keys = [
+              {
+                key = "Enter";
+                mods = "ALT";
+                action = lib.generators.mkLuaInline "wezterm.action.DisableDefaultAssignment";
+              }
+            ];
           };
-          keys = [
-            {
-              key = "Enter";
-              mods = "ALT";
-              action = lib.generators.mkLuaInline "wezterm.action.DisableDefaultAssignment";
-            }
-          ];
         };
       };
-    };
   };
 }
