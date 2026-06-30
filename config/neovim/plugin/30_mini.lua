2c2b3deda25b95a58de29d63c703780884a1b474
AGN907 <ahmednahdi907@gmail.com>
2026-06-30 23:14:37 +0300

    feat(nvim): add toggle hidden, splits, and other keymaps for mini-files buffers

--- config/neovim/plugin/30_mini.lua
+++ config/neovim/plugin/30_mini.lua
@@ -242,6 +242,70 @@ nixInfo.lze.load({
 					width_preview = 30,
 				},
 			})
+
+			local show_dotfiles = true
+
+			local filter_show = function(fs_entry)
+				return true
+			end
+
+			local filter_hide = function(fs_entry)
+				return not vim.startswith(fs_entry.name, ".")
+			end
+
+			local toggle_dotfiles = function()
+				show_dotfiles = not show_dotfiles
+				local new_filter = show_dotfiles and filter_show or filter_hide
+				MiniFiles.refresh({ content = { filter = new_filter } })
+			end
+
+			local set_cwd = function()
+				local path = (MiniFiles.get_fs_entry() or {}).path
+				if path == nil then
+					return vim.notify("Cursor is not on valid entry")
+				end
+				vim.fn.chdir(vim.fs.dirname(path))
+			end
+
+			local yank_path = function()
+				local path = (MiniFiles.get_fs_entry() or {}).path
+				if path == nil then
+					return vim.notify("Cursor is not on valid entry")
+				end
+				vim.fn.setreg(vim.v.register, path)
+			end
+
+			local ui_open = function()
+				vim.ui.open(MiniFiles.get_fs_entry().path)
+			end
+
+			local map_split = function(buf_id, lhs, direction)
+				local rhs = function()
+					local cur_target = MiniFiles.get_explorer_state().target_window
+					local new_target = vim.api.nvim_win_call(cur_target, function()
+						vim.cmd(direction .. " split")
+						return vim.api.nvim_get_current_win()
+					end)
+
+					MiniFiles.set_target_window(new_target)
+				end
+
+				local desc = "Split " .. direction
+				vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+			end
+
+			Config.new_autocmd("User", "MiniFilesBufferCreate", function(args)
+				local buf_id = args.data.buf_id
+				vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+
+				vim.keymap.set("n", "g~", set_cwd, { buffer = b, desc = "Set cwd" })
+				vim.keymap.set("n", "gX", ui_open, { buffer = b, desc = "OS open" })
+				vim.keymap.set("n", "gy", yank_path, { buffer = b, desc = "Yank path" })
+
+				map_split(buf_id, "<C-s>", "belowright horizontal")
+				map_split(buf_id, "<C-v>", "belowright vertical")
+				map_split(buf_id, "<C-t>", "tab")
+			end)
 		end,
 	},
 	{
