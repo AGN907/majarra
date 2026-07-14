36671805b2172e4a8d8967febfa81375d16a3732
AGN907 <ahmednahdi907@gmail.com>
2026-07-14 06:36:24 +0300

    feat(cli): enable fzf module

--- modules/apps/cli.nix
+++ modules/apps/cli.nix
@@ -9,6 +9,7 @@
       atuin
       btop
       bat
+      fzf
     ];
     base.homeManager =
       { pkgs, ... }:
@@ -141,5 +142,27 @@
           ];
         };
       };
+    fzf.homeManager = { config, lib, ... }: {
+      programs.fzf = {
+        enable = true;
+        enableFishIntegration = false;
+        colors =
+          with config.lib.stylix.colors.withHashtag;
+          lib.mkForce {
+            "bg" = base00;
+            "bg+" = base02;
+            "fg" = base05;
+            "fg+" = base05;
+            "header" = base0E;
+            "hl" = base08;
+            "hl+" = base08;
+            "info" = base0A;
+            "marker" = base06;
+            "pointer" = base06;
+            "prompt" = base0E;
+            "spinner" = base06;
+          };
+      };
+    };
   };
 }
