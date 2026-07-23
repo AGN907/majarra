fd89b2fbcc66767aabd9d00d06ed648072cf78f9
AGN907 <ahmednahdi907@gmail.com>
2026-07-23 15:04:39 +0300

    feat(zen): set mime apps

--- modules/apps/zen/default.nix
+++ modules/apps/zen/default.nix
@@ -35,5 +35,39 @@
         policies = import ./_polices.nix;
         profiles.default = import ./_profile-default.nix { inherit pkgs; };
       };
+
+      xdg.mimeApps =
+        let
+          associations = builtins.listToAttrs (
+            map
+              (name: {
+                inherit name;
+                value = "zen-twilight.desktop";
+              })
+              [
+                "application/x-extension-shtml"
+                "application/x-extension-xhtml"
+                "application/x-extension-html"
+                "application/x-extension-xht"
+                "application/x-extension-htm"
+                "x-scheme-handler/unknown"
+                "x-scheme-handler/mailto"
+                "x-scheme-handler/chrome"
+                "x-scheme-handler/about"
+                "x-scheme-handler/https"
+                "x-scheme-handler/http"
+                "application/xhtml+xml"
+                "application/json"
+                "text/plain"
+                "text/html"
+              ]
+          );
+        in
+        {
+          enable = true;
+          associations.added = associations;
+          defaultApplications = associations;
+        };
     };
+
 }
