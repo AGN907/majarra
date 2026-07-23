095ca923f28774d2f567ba61948fcc85036de266
AGN907 <ahmednahdi907@gmail.com>
2026-07-23 15:12:32 +0300

    feat(direnv): toggle silent output

--- modules/apps/cli.nix
+++ modules/apps/cli.nix
@@ -49,6 +49,7 @@
           direnv = {
             enable = true;
             nix-direnv.enable = true;
+            silent = true;
             config.global.warn_timeout = 0;
           };
           tealdeer = {
